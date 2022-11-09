0b8d4da92db06f62f27c3103c0f356f3cbc601d3
Glaucco <glauccolima@gmail.com>
2022-11-09 17:31:59 -0300

    Correção nota mínima

--- main.js
+++ main.js
@@ -26,16 +26,16 @@ function adicionaLinha() {
         alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`);
     } else {
         atividades.push(inputNomeAtividade.value);
-    notas.push(parseFloat(inputNotaAtividade.value));
+        notas.push(parseFloat(inputNotaAtividade.value));
 
-    let linha = '<tr>';
-    linha += `<td>${inputNomeAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
-    linha += '</tr>';
+        let linha = '<tr>';
+        linha += `<td>${inputNomeAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
+        linha += '</tr>';
 
-    linhas += linha;
-}
+        linhas += linha;
+    }
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
 }
@@ -49,7 +49,7 @@ function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
     document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? spanAprovado : spanReprovado;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 
 }
 
